fda9ed5a5ab87060ba15f54f3080bffe9220507e
Abdullah Naveed <70411864+Abdullahnaveed102@users.noreply.github.com>
2023-10-20 23:02:02 -0500

    Final from my side
    Please test this and maybe verify the output with with the TA

--- lab/lab2_an.py
+++ lab/lab2_an.py
@@ -13,6 +13,14 @@ def PrintCCode(ir):
 	print(code,'\n')
 
 #===========================================================>
+# for (int _l0 = 0; _l0 < 20; _l0 += 1) {
+# for (int _l1 = _l0 + 3; _l1 < _l0 + 10; _l1 += 1) {
+# for (int _l2 = _l1 + 20; _l2 < _l1 + 30; _l2 += 1) {
+# A[_l0 + 1][_l1][_l2] = B[_l0][_l1][_l2] + 2;
+# } 
+# } 
+# }
+#===========================================================>
 def Loop0():
     ir = []
 
@@ -43,8 +51,76 @@ def Loop0():
     ir.extend([loopi])
 
     return ir
-#===========================================================>
 
+#===========================================================>
+# for (int _l3 = 0; _l3 < L; _l3 += 1) {
+# for (int _l4 = 0; _l4 < M; _l4 += 1) {
+# for (int _l5 = 0; _l5 < N; _l5 += 1) {
+# A[_l5 + 1][_l4 + 1][_l3] = A[_l5][_l4][_l3] + A[_l5][_l4 + 1][_l3 + 1];
+# } 
+# } 
+# } 
+#===========================================================>
+def Loop1():
+    ir = []
+
+    L = Scalar('int', 'L')
+    M = Scalar('int', 'M')
+    N = Scalar('int', 'N')
+    A = Ndarray('int', (N, M, L), 'A')
+
+    loopk = Loop(0, L, 1, [])
+    loopj = Loop(0, M, 1, [])
+    loopi = Loop(0, N, 1, [])
+    loopk.body.append(loopj)
+    loopj.body.append(loopi)
+
+    lhs = Index(Index(Index(A, Expr(loopi.iterate, 1, '+')),
+                Expr(loopj.iterate, 1, '+')), loopk.iterate)
+    rhs1 = Index(Index(Index(A, loopi.iterate), loopj.iterate), loopk.iterate)
+    rhs2 = Index(Index(Index(A, loopi.iterate), Expr(
+        loopj.iterate, 1, '+')),  Expr(loopk.iterate, 1, '+'))
+
+    body = Assignment(lhs, Expr(rhs1, rhs2, '+'))
+    loopi.body.append(body)
+
+    ir.extend([Decl(L)])
+    ir.extend([Decl(M)])
+    ir.extend([Decl(N)])
+    ir.extend([Decl(A)])
+    ir.extend([loopk])
+
+    return ir
+
+#===========================================================>
+# for (int _l6 = 0; _l6 < N; _l6 += 1) {
+# for (int _l7 = 0; _l7 < N; _l7 += 1) {
+# A[_l6][_l7] = A[_l6 + 1][_l7 - 1];
+# } 
+# } 
+#===========================================================>
+def Loop2():
+    ir = []
+
+    N = Scalar('int', 'N')
+    A = Ndarray('int', (N, N), 'A')
+
+    loopi = Loop(0, N, 1, [])
+    loopj = Loop(0, N, 1, [])
+
+    loopi.body.append(loopj)
+
+    lhs = Index(Index(A, loopi.iterate), loopj.iterate)
+    rhs = Index(Index(A, Expr(loopi.iterate, 1, '+')),
+                Expr(loopj.iterate, 1, '-'))
+
+    loopj.body.append(Assignment(lhs, rhs))
+
+    ir.extend([Decl(N)])
+    ir.extend([Decl(A)])
+    ir.extend([loopi])
+
+    return ir
 
 #===========================================================>
 #===========================================================>
@@ -150,12 +226,12 @@ def merge(tiled_loops, point_loops):
 #===========================================================>
 #===========================================================> LoopTiling
 def LoopTiling(ir, tile_size = []):
-    new_ir = []
+    new_ir = ir
     low_bounds = []
     up_bounds = []
     body = ""
     #====================================> lower_bounds, upper_bounds change
-    for ir_item in ir:
+    for ir_item in new_ir:
         if type(ir_item) == Loop:
             org_lower_bounds, org_upper_bounds, index_dict = GetKeyInfo(ir_item)
             i = 0
@@ -174,35 +250,36 @@ def LoopTiling(ir, tile_size = []):
     
     point_loops_ir = point_loops(org_lower_bounds,org_upper_bounds,index_dict, tile_size, len(low_bounds),body)
     
-    for ir_item in ir:
+    for ir_item in new_ir:
         if type(ir_item) == Loop:
             merge(ir_item,point_loops_ir)
-    
-    
        
-    # ====================================> Printing
-    print("============================> New Lower Bounds")
-    PrintCCode(low_bounds)
+    # ========================================================================> Printing
+    # print("============================> New Lower Bounds")
+    # PrintCCode(low_bounds)
     
-    print("============================> New Upper Bounds")
-    PrintCCode(up_bounds)
+    # print("============================> New Upper Bounds")
+    # PrintCCode(up_bounds)
     
-    print("============================> New Points Loops Generated")
-    PrintCCode(point_loops_ir)  
+    # print("============================> New Points Loops Generated")
+    # PrintCCode(point_loops_ir)  
     
-    print("============================> Inner Most Statements")
-    PrintCCode(body)
+    # print("============================> Inner Most Statements")
+    # PrintCCode(body)
     
-    print("============================> Merged Final Loops")
-    PrintCCode(ir)
-    
-    # return new_ir
+    # print("============================> Merged Final Loops")
+    # PrintCCode(ir)
+    # ========================================================================>
+    return new_ir
             
 	
 if __name__ == "__main__":
     print("============================> Original code")
-    loop0_ir = Loop0()  # Loop0 is just an example
+    loop0_ir = Loop0()  
+    loop1_ir = Loop1()
+    loop2_ir = Loop2()
     PrintCCode(loop0_ir)
 
-    loop0_ir_after_tiling = LoopTiling(loop0_ir, tile_size = [3,4,5])
-    # PrintCCode(loop0_ir_after_tiling)
+    print("============================> Updated code")
+    returned_ir = LoopTiling(loop0_ir, tile_size = [3,4,5])
+    PrintCCode(returned_ir)
